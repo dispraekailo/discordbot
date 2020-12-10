8781ded71a8cacdb4e10c88e84a14bf296e9627b
dispraekailo <58571694+dispraekailo@users.noreply.github.com>
2020-12-10 17:51:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 import os
 import random
 from discord.ext import commands
-from config import settings
 from discord import Game
 from ctypes.util import find_library
 from discord import opus
